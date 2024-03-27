42b65c123ff208b7ecbef4379928f34086879fd9
Muhammad Shoaib <m.xhoaib1@gmail.com>
2024-03-27 11:57:48 +0500

    slider fixation

--- app/_components/ShopProduct/index.tsx
+++ app/_components/ShopProduct/index.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 import Image, { StaticImageData } from "next/image";
 import { ButtonContained, ButtonOutlined, LabeledAvatarGroup } from "..";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
 
 interface IShopProduct {
@@ -20,36 +20,34 @@ interface IShopProduct {
 }
 
 const ShopProduct = (props: IShopProduct) => {
-  const {
-    title,
-    text,
-    direction,
-    avatarData,
-    productImage,
-  } = props;
-  const router = useRouter()
+  const { title, text, direction, avatarData, productImage } = props;
+  const router = useRouter();
   return (
     <div
-      className={` flex items-center flex-col-reverse lg:flex-row gap-10 xl:gap-[80px] bg-[#111] rounded-2xl px-8 lg:px-[60px] m-[80px] section-margin pb-8 lg:pb-0 ${direction === "reverse" ? 'lg:!flex-row-reverse' : 'flex-col'}`}
+      className={` flex items-center flex-col-reverse lg:flex-row gap-10 xl:gap-[80px] bg-[#111] rounded-2xl px-8 lg:px-[60px] m-[80px] section-margin pb-8 lg:pb-0 ${
+        direction === "reverse" ? "lg:!flex-row-reverse" : "flex-col"
+      }`}
     >
       <div className="md:pb-10 md:pt-7 lg:pt-10 lg:pb-14 basis-full xl:basis-[55%]">
         <h2 className="relative section-heading-44 uppercase font-normal golden-gradient-text">
           {title}
           <motion.span
             initial={{ height: `100%` }}
-            whileInView={{ height: '0px' }}
+            whileInView={{ height: "0px" }}
             transition={{ duration: 2, delay: 0.4 }}
             viewport={{ once: true }}
-            className={`absolute bg-[#111111] inset-0`}></motion.span>
+            className={`absolute bg-[#111111] inset-0`}
+          ></motion.span>
         </h2>
         <p className="pb-6 pt-4 relative">
           {text}
-        <motion.span
+          <motion.span
             initial={{ height: `100%` }}
-            whileInView={{ height: '0px' }}
+            whileInView={{ height: "0px" }}
             transition={{ duration: 2, delay: 0.5 }}
             viewport={{ once: true }}
-            className={`absolute bg-[#111111] inset-0`}></motion.span>
+            className={`absolute bg-[#111111] inset-0`}
+          ></motion.span>
         </p>
         <LabeledAvatarGroup
           width={34}
@@ -58,19 +56,31 @@ const ShopProduct = (props: IShopProduct) => {
           caption={avatarData?.caption}
         />
         <div className="flex relative items-center flex-col md:flex-row gap-8 pt-8 xl:w-[70%]">
-          <ButtonContained onClick={() => { router.push('/shop/slug') }}>Buy Now!</ButtonContained>
-          <ButtonOutlined onClick={() => { router.push('/shop/slug') }}>Shop Now</ButtonOutlined>
+          <ButtonContained
+            onClick={() => {
+              router.push("/shop/slug");
+            }}
+          >
+            Buy Now!
+          </ButtonContained>
+          <ButtonOutlined
+            onClick={() => {
+              router.push("/shop/slug");
+            }}
+          >
+            Shop Now
+          </ButtonOutlined>
           <motion.span
             initial={{ height: `100%` }}
-            whileInView={{ height: '0px' }}
+            whileInView={{ height: "0px" }}
             transition={{ duration: 2, delay: 0.7 }}
             viewport={{ once: true }}
-            className={`absolute bg-[#111111] inset-0`}></motion.span>
+            className={`absolute bg-[#111111] inset-0`}
+          ></motion.span>
         </div>
       </div>
       <div className="flex items-center justify-center w-full h-full basis-full xl:basis-[40%]">
-        <div
-          className="scale-[1.2] lg:scale-[1.9] xl:scale-[2.7] ">
+        <div className="scale-[1.2] lg:scale-[1.25] xl:scale-[1.35] ">
           <motion.figure
             initial={{ scale: 0, opacity: 0 }}
             whileInView={{ scale: 1, opacity: 1 }}
--- app/_components/Slider/index.tsx
+++ app/_components/Slider/index.tsx
@@ -1,26 +1,82 @@
 "use client";
-import "swiper/css";
-import "swiper/css/navigation";
+import { useState } from "react";
+import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectFade, Navigation } from "swiper/modules";
 import FeatureProductCard from "../FeatureProductCard";
+import { LeftNavigationIcon, RightNavigationIcon } from "@/public/assets";
+import "swiper/css";
+import "swiper/css/navigation";
 
 export default function Slider({ slides = [] }: { slides: any[] }) {
+  const [swiper, setSwiper] = useState<any>(null);
+  const [currentSlide, setCurrentSlide] = useState<any>(1);
+
+  const goNext = () => {
+    if (swiper) {
+      swiper?.slideNext();
+    }
+  };
+
+  const goPrev = () => {
+    if (swiper) {
+      swiper?.slidePrev();
+    }
+  };
+
+  const handleSlideChange = () => {
+    if (swiper) {
+      setCurrentSlide(swiper.activeIndex + 1);
+    }
+  };
+
   return (
-    <div className="w-full">
+    <div className="w-full relative">
       <Swiper
         slidesPerView={1}
         spaceBetween={30}
         navigation={false}
         modules={[EffectFade, Navigation]}
-        className=""
+        onSwiper={setSwiper}
+        onSlideChange={handleSlideChange}
       >
-        {slides?.map((slide) => (
-          <SwiperSlide className="">
+        {slides?.map((slide, index) => (
+          <SwiperSlide key={index} className="">
             <FeatureProductCard />
           </SwiperSlide>
         ))}
       </Swiper>
+
+      <div className="absolute top-[5%] z-50 right-[17%] lg:mt-10 gap-2 lg:gap-4">
+        <button
+          onClick={goPrev}
+          className={`p-2 ${currentSlide === 1 && "cursor-not-allowed"}`}
+        >
+          <p className="text-white">
+            {currentSlide === 1 ? "---" : currentSlide - 1}
+          </p>
+          <Image
+            src={LeftNavigationIcon}
+            alt="left-navigation"
+            style={{ width: "auto", height: "auto" }}
+          />
+        </button>
+        <button
+          onClick={goNext}
+          className={`p-2 ${
+            currentSlide === slides?.length && "cursor-not-allowed"
+          }`}
+        >
+          <p className="text-white">
+            {currentSlide === slides?.length ? "---" : currentSlide + 1}
+          </p>
+          <Image
+            src={RightNavigationIcon}
+            alt="right-navigation"
+            style={{ width: "auto", height: "auto" }}
+          />
+        </button>
+      </div>
     </div>
   );
 }
